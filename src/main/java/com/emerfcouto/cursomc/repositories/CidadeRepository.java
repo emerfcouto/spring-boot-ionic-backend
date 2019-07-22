1abe4384f0c852c6803c589a2fd4bfe07c94294f
Emerson F Couto <emerfcouto@hotmail.com>
2019-07-21 21:04:43 -0300

    Estado e Cidade

---
+++ src/main/java/com/emerfcouto/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.emerfcouto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emerfcouto.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
