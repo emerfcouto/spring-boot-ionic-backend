2579b303f34d8e76de534762b9e872bd00d8cd6f
Emerson F Couto <emerfcouto@hotmail.com>
2019-07-24 22:15:13 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/emerfcouto/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.emerfcouto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emerfcouto.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
