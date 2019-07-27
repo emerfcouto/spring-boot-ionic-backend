c078f6aa7a89ab62f66a3083f1571405d7a3120d
Emerson F Couto <emerfcouto@hotmail.com>
2019-07-27 14:29:18 -0300

    Listando todas categorias com DTO

--- src/main/java/com/emerfcouto/cursomc/services/CategoriaService.java
+++ src/main/java/com/emerfcouto/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.emerfcouto.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -50,4 +52,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
+
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
+	}
+	
 }
