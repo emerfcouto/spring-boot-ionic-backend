d7526288d5c4f2e352facc7fdfb0c998aa78b237
Emerson F Couto <emerfcouto@hotmail.com>
2019-07-27 20:51:24 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/emerfcouto/cursomc/services/CategoriaService.java
+++ src/main/java/com/emerfcouto/cursomc/services/CategoriaService.java
@@ -42,9 +42,11 @@ public class CategoriaService {
 	
 	public Categoria update(Categoria obj) {
 		
-		this.find(obj.getId());
+		Categoria newObj = find(obj.getId());
 		
-		return repo.save(obj);
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -72,5 +74,10 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		
 		return new Categoria(objDto.getId(), objDto.getNome());		
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
 	}
 }
