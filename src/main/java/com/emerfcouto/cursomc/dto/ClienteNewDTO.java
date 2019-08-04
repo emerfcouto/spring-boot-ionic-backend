a24d84cf0878c376656718b3053804db935091a0
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-03 21:47:29 -0300

    Adicionando senha a Cliente

--- src/main/java/com/emerfcouto/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/emerfcouto/cursomc/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable {
 	private Integer tipo;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
@@ -84,6 +87,14 @@ public class ClienteNewDTO implements Serializable {
 		this.tipo = tipo;
 	}
 
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 	public String getLogradouro() {
 		return logradouro;
 	}
@@ -155,5 +166,4 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
 }
