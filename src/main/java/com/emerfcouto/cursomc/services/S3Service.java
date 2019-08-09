328061b45547736316d5e6b419e17566dd47e4ae
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-08 21:00:17 -0300

    Tratando exceções adequadamente

--- src/main/java/com/emerfcouto/cursomc/services/S3Service.java
+++ src/main/java/com/emerfcouto/cursomc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.emerfcouto.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -38,7 +39,7 @@ public class S3Service {
 		
 		} catch (IOException e) {
 			
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 	}
 
@@ -58,7 +59,7 @@ public class S3Service {
 		
 		} catch (URISyntaxException e) {
 			
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
